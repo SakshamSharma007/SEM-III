3cb3cee83596f188a97e83e96fe9ccc087d60a5d
insane_banda <piyushkeshari24@gmail.com>
2020-07-04 09:51:29 +0530

    Done

---
+++ DBMS/Assignment 1/Exercise 2/Employee.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Employee
+(
+	empno INTEGER,
+    ename VARCHAR(20),
+    job VARCHAR(20),
+    mgr INTEGER,
+    hiredate DATE,
+    sal INTEGER,
+    comm INTEGER,
+    dept INTEGER
+);
+
+-- INSERT INTO Dept
+-- VALUES
+-- 	   (1,'ACCOUNTING','ST LOUIS'),
+--     (2,'RESEARCH','NEW YORK'),
+--     (3,'SALES','ATLANTA'),
+--     (4,'OPERATIONS','SEATTLEdept');
