072aaa0dab180c0cc1173addedd19a5fd568e11f
insane_banda <piyushkeshari24@gmail.com>
2020-07-04 09:46:39 +0530

    Create Dept.sql

---
+++ DBMS/Assignment 1/Exercise 2/Dept.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Dept
+(
+	deptno INTEGER,
+    dname VARCHAR(20),
+    loc VARCHAR(20)
+);
+
+INSERT INTO Dept
+VALUES
+	(1,'ACCOUNTING','ST LOUIS'),
+    (2,'RESEARCH','NEW YORK'),
+    (3,'SALES','ATLANTA'),
+    (4,'OPERATIONS','SEATTLEdept');
